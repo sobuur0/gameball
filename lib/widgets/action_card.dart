fd21de76d5a4722330bcb6d60b3b4c41fd3b8fc2
t-bolt <isholasobuur@gmail.com>
2022-03-24 18:18:13 +0100

    added action_cards to sign_up_page

--- lib/widgets/action_card.dart
+++ lib/widgets/action_card.dart
@@ -5,31 +5,44 @@ class ActionCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12.0),
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20.0,
+        vertical: 20.0,
       ),
-      child: Row(
-        children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Icon(Icons.money),
-              Text(
-                'Earn',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              )
+      child: Container(
+        height: 150,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Icon(Icons.money),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    'Earn',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+              const Icon(
+                Icons.cancel,
+              ),
             ],
           ),
-          const Icon(
-            Icons.cancel,
-          ),
-        ],
+        ),
       ),
     );
   }
