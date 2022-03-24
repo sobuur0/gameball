75acbc9d99d5b7d38ca67b3c02c0ebb3d0ed7233
t-bolt <isholasobuur@gmail.com>
2022-03-24 13:18:47 +0100

    comleted dashboard ui

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,15 +10,18 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return SafeArea(
       child: Scaffold(
         body: Container(
-          height: 200.0,
-          decoration: const BoxDecoration(
+          height: size.height*0.25,
+          decoration: BoxDecoration(
             //TODO: change to required color
-            color: Colors.yellow,
+            color: Colors.yellow.shade800,
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Padding(
                 padding: const EdgeInsets.symmetric(
@@ -37,18 +40,28 @@ class _HomepageState extends State<Homepage> {
                   ],
                 ),
               ),
-              // const Text.rich(
-              //   TextSpan(
-              //     children: [
-              //       TextSpan(
-              //         text: 'Welcome to\n',
-              //       ),
-              //       TextSpan(
-              //         text: 'Plus Rewards',
-              //       ),
-              //     ],
-              //   ),
-              // ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12.0,),
+                child: Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Welcome to\n',
+                        style: TextStyle(
+                          fontSize: 14.0
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'Plus Rewards',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -59,11 +72,11 @@ class _HomepageState extends State<Homepage> {
   Widget _buildFaqButton() {
     return Container(
       height: 40.0,
-      width: 70.0,
+      width: 60.0,
       decoration: BoxDecoration(
         //TODO: change to required color
         borderRadius: BorderRadius.circular(8.0),
-        color: Colors.white,
+        color: Colors.yellow.shade700,
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
