b0042689221ac1208f53f3954d888f2878a9a16e
t-bolt <isholasobuur@gmail.com>
2022-03-30 12:11:12 +0100

    implement referral_stages

--- lib/pages/referral_page.dart
+++ lib/pages/referral_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gameball/widgets/referral_stages.dart';
 
 class ReferralPage extends StatefulWidget {
   const ReferralPage({Key? key}) : super(key: key);
@@ -28,8 +29,49 @@ class _ReferralPageState extends State<ReferralPage> {
         ),
       ),
       body: Column(
-        children: <Widget>[
-
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 10.0,
+            ),
+            child: Text(
+              'We value friendship',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 5.0,
+            ),
+            child: Text(
+              'Follow the steps below and get rewarded',
+              style: TextStyle(
+                fontSize: 14.0,
+              ),
+            ),
+          ),
+          ReferralStages(
+            topPadding: 30.0,
+            stepNumber: '1',
+            stepDesc: 'Share your link',
+            stepIcon: Icons.copy,
+          ),
+          ReferralStages(
+            topPadding: 0.0,
+            stepNumber: '2',
+            stepDesc: 'Your friend signup using your link',
+          ),
+          ReferralStages(
+            topPadding: 0.0,
+            stepNumber: '3',
+            stepDesc: 'Your friend places an order',
+          ),
         ],
       ),
     );
