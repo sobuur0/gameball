a7fbfa9502814604b5f288c6a9be194ecab6f7af
t-bolt <isholasobuur@gmail.com>
2022-04-16 21:17:42 +0100

    refactor

--- lib/pages/referral_page.dart
+++ lib/pages/referral_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gameball/widgets/copy_referral_container.dart';
 import 'package:gameball/widgets/referral_stages.dart';
